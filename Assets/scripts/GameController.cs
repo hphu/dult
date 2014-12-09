cbefa31f675f268ee0aa59a72c780d367690091a
Henry Phu <hphu@bu.edu>
2014-12-08 21:22:08 -0500

    default background, added changing color to main light

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -11,13 +11,30 @@ public class GameController : MonoBehaviour
 	public float startWait;
 	public float waveWait;
 	public GUIText scoreText;
+	public Light mainLight;
 	private int score;
-	
+	private Color[] mainLightColors;
+	private int colorCounter = 0;
+
 	void Start()
 	{
 		score = 0;
+		mainLightColors = new Color[5];
+		mainLightColors [0] = new Color (.8f, .3f, .3f, 1f);//red
+		mainLightColors [1] = new Color (.55f, .94f, .97f, 1f); //cyan
+		mainLightColors [2] = new Color (.27f, .41f, .92f, 1f);//blue
+		mainLightColors [3] = new Color (.32f, .96f, .56f, 1f);//green
+		mainLightColors [4] = new Color (1f, .68f, 0f, 1f);//orange
+
 		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update(){
+		this.mainLight.color = Color.Lerp (this.mainLight.color, mainLightColors[(colorCounter+1) % mainLightColors.Length], .015f);
+		if (this.mainLight.color == mainLightColors [(colorCounter + 1) % mainLightColors.Length]) {
+			colorCounter = (colorCounter + 1) % mainLightColors.Length;
+		}
 	}
 	
 	IEnumerator SpawnWaves ()
